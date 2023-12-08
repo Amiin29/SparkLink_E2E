19d8793c7aaeef56eb092bc68d6ca31d5069602a
Amiin29 <Amiin29@users.noreply.github.com>
2023-12-08 15:38:56 +0000

    deploy: e9f8015a1959485c6444d87363c8615aa97ac43a

--- cypress/integration/locators/OsDevicesLocators/LocatorsOsDevicesByAndroid.js
+++
@@ -1,6 +0,0 @@
-export class LocatorsOsDevicesByAndroid{
-
-    DevicesButton= ".small-tab-buttons > :nth-child(2)"
-    ButtonScreenResolutions = ".small-tab-buttons > :nth-child(3)"
-    ButtonDeviceLanguages = ".small-tab-buttons > :nth-child(4)"
-   }
